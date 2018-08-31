634354da6632948314356a57d7eb62fda9087c5c
Yi Cheng <i.albert.cheng@gmail.com>
2018-08-31 23:15:08 +0100

    finish exercise NN back propagation and regularization

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,22 +62,116 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% ==================================================================================
+% Part 1: Feedforward the neural network and return the cost in the
+%         variable J. After implementing Part 1, you can verify that your
+%         cost function computation is correct by verifying the cost
+%         computed in ex4.m
+
+%Theta1 25 401
+%Theta2 10 26
+%hidden_layer_size 25
+%X 5000 400
+%forward propagation to calcualte h(X) which is also a(3), the output layer 
+
+%1.0 cost
+
+%layer 1
+%a1 5000 401
+a1 = [ones(m,1) X];
+%z2 5000 25
+z2 = a1*Theta1';
+
+%z2 5000 25
+a2 = sigmoid(z2);
+%z2 5000 26
+a2 = [ones(m,1) a2];
+
+%5000*10
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+
+%convert the 5000*1's y vector into a 10*5000 matrix
+%where each column is a vector represent the original value
+yk = zeros(num_labels, m);
+for i=1:m
+    %for each column, set the orignal value to be 1
+    %for example, 1,2,5
+    % then yk(1,1) = 1, yk(2,2) = 1, yk(5,3) = 1
+    yk(y(i),i) = 1;
+end
+% yk
+
+%(10*5000)' dot product  (5000*10)
+%and result is till a 5000*10
+%dot product does element wise multiplication 
+% j_matrix = (-yk)' .* log(a3) - (1-yk)' .* log(1-a3)
+% j_vector = sum(j_matrix)
+% j_value = sum(j_vector)
+% J = 1/m * j_value
+
+J = 1/m * sum(sum( (-yk)' .* log(a3) - (1-yk)' .* log(1-a3) ));
+
+%1.1 regularization
+regulator = (lambda /(2*m)) * ( sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) );
+
+J = J + regulator;
+
+%Part 2 ==================================================================================
+%back propagation
+
+%Theta1 25 401
+%Theta2 10 26
+%hidden_layer_size 25
+%X 5000 400
+
+%compute for each training example 
+for t = 1:m
+    %401 * 1
+    a1 = [1; X(t,:)'];
+    %z2 25 * 1 
+    z2 = Theta1*a1;
+
+    %z2 25 1
+    a2 = sigmoid(z2);
+    %26 1
+    a2 = [1; a2];
+
+    %10*1
+    z3 = Theta2*a2;
+    a3 = sigmoid(z3);
+    
+   
+    %yk(:,t) is a 10 * 1 vector 
+    %so a3 is 10*1
+    d3 = a3 - yk(:,t);
+    
+    %26*10 * 10*1 = 26*1
+    d2 = (Theta2' * d3) .* [1; sigmoidGradient(z2)];
+    %25*1
+    d2 = d2(2:end); % no need for bias row for delta
+    
+    % Big delta accumulate 
+    %d2 * a1' = 25*1 by 1*401 = 25*401 which is the same size as Theta1, so it can do +
+    %d3 * a2' = 10*1 by 1*26 = 10*26 which is the same size as Theta2, so it can do +
+    Theta1_grad = Theta1_grad + d2 * a1';
+    Theta2_grad = Theta2_grad + d3 * a2';
+
+    
+end    
+
+%not regularized
+% Theta1_grad = (1/m) * Theta1_grad;
+% Theta2_grad = (1/m) * Theta2_grad;
 
 
+%regularized
+Theta1_grad(:, 1) = Theta1_grad(:, 1) ./ m;
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) ./ m + ((lambda/m) * Theta1(:, 2:end));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+Theta2_grad(:, 1) = Theta2_grad(:, 1) ./ m;
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) ./ m + ((lambda/m) * Theta2(:, 2:end));
 
 
 % -------------------------------------------------------------
